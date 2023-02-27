9e9c01cdc20b101504cdf58f9571ca6fd43b2267
Roxy Osorio <roxy.osorio@moo.com>
2023-02-27 13:28:54 -0500

    README

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,7 @@ function writePassword() {
 
   var password = generatePassword();
   var passwordEl = document.querySelector("#password");
-  // sanity check
+  // Sanity check
   console.log(passwordEl)
   console.dir(passwordEl)
   passwordEl.value = password;
@@ -34,7 +34,7 @@ function generatePassword() {
   var askLowerCase = window.confirm("Confirm if You Would Like to Include Lowercase Characters?");
   var askNums = window.confirm("Confirm if You Would Like to Include Numbers?");
 
-  // sanity check
+  // Sanity check
   console.log(numOfChars)
   console.log(askSpecChars)
   console.log(askUpperCase)
@@ -42,11 +42,7 @@ function generatePassword() {
   console.log(askLowerCase)
 
 if (!askSpecChars && !askUpperCase && !askLowerCase){alert("You must choose at least one criteria!")};
-  // if they say no to specChar && upperCase etc
-  // alert them the choose at least one
-  // stop the app
-  // return '';
-  // do a thing
+
 
   var specChars = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+'];
   var allUpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -79,13 +75,7 @@ var ranPassword = "";
     console.log(ranPassword);
   }
 
-  // for each char the user wants
-  // -- pick a random number
-  // -- use that number to grab a rand char out of the chosenOptions
-  // -- add that char to a string
 
-
-  // return that string
   return ranPassword;
 }
 
@@ -93,14 +83,3 @@ var ranPassword = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-// generateBtn.querySelector('#bgnBtn').addEventListener('click', promptMe);
-
-
-// function promptMe() {
-//     var userAdjective = prompt("Please provide an Adjective");
-//     alert (userAdjective);
-// }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Password-Generator
+
+## Description
+
+This is a password generator website. It will create a secure and random password that will make it very hard for someone to guess. When clicking the 'Generate' button, the website will prompt and ask you how how long you would like your password from 8 to 128 characters and if you would like to include Special Characters, Uppercase Letters, Lowercase Letters and/or Numbers. After selecting the criteria, you will be presented with your new password!
+
+After completing this assignment, I must say I can read Javascript a whole lot better now!
+
+## Installation
+
+N/A
+
+## Usage
+
+The screenshots below show the generated password and a console log on the right that shows what the user chose and how the program added each random character to the password depending on what the user chose. 
+
+Screenshot
+
+![alt text](./PasswordGen.png)
+
+Link to Website is below
+
+
+## Credits
+
+N/A
+
